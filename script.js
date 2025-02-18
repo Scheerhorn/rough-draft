f5cc4a7889e0596b3d0ca56e28cff69da15f3c5c
Scheerhorn <daniel.scheerhorn.wolfe@gmail.com>
2025-02-18 15:53:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,76 @@
 const dailyDeals = {
   0: [ // Sunday
-        // RIDGELAND 
-      { name: "Open Mind", deal: "&#127968 15% off store-wide", lat: 32.38819, lon:-90.11402},
-      // Rootdown - Old Canton is closed on Sunday
-      // Ignite need to check on Sunday 
-      // Southern Sky - Lakeland is closed on Sunday
+        //CENTRAL
+          // RIDGELAND 
+            { name: "Open Mind", deal: "&#127968 15% off store-wide", lat: 32.38819, lon:-90.11402},
+            // Rootdown - Old Canton is closed on Sunday
+            // Ignite need to check on Sunday 
+            // Southern Sky - Lakeland is closed on Sunday
+            // Rootdown - Lakeland is closed on Sunday
+          //PEARL
+            { name: "", deal: "", lat: 0, lon: 0},
+            
+            
   ],
   1: [ // Monday
         // RIDGELAND
-      { name: "Open Mind", deal: "&#127851 20% off Edibles", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#127804 20% off select micros Flower", lat: 32.36077, lon: -90.14643},
-      { name: "Ignite", deal: "&#127851 20% off Edibles", lat: 32.3766, lon: -90.1472},
-      { name: "Southern Sky - Lakeland", deal: "&#127851 20% off Edibles", lat: 32.3329, lon: -90.1386}
+          { name: "Open Mind", deal: "&#127851 20% off Edibles", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#127804 20% off select micros Flower", lat: 32.36077, lon: -90.14643},
+          { name: "Ignite", deal: "&#127851 20% off Edibles", lat: 32.3766, lon: -90.1472},
+          { name: "Southern Sky - Lakeland", deal: "&#127851 20% off Edibles", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#127804 20% off select micros Flower", lat: 32.33369, lon: -90.1446},
+          { name: "", deal: "", lat: 0, lon: 0},
   ],
   2: [ // Tuesday
-    // RIDGELAND
-      { name: "Open Mind", deal: "&#128684 20% off Pre-rolls", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.36077, lon: -90.14643},
-      { name: "Ignite", deal: "&#128167 20% off all Tincture", lat: 32.3766, lon: -90.1472},
-      { name: "Southern Sky - Lakeland", deal: "&#9889 20% off Vapes", lat: 32.3329, lon: -90.1386}
+        // RIDGELAND
+          { name: "Open Mind", deal: "&#128684 20% off Pre-rolls", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.36077, lon: -90.14643},
+          { name: "Ignite", deal: "&#128167 20% off all Tincture", lat: 32.3766, lon: -90.1472},
+          { name: "Southern Sky - Lakeland", deal: "&#9889 20% off Vapes", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.33369, lon: -90.1446},
+          { name: "", deal: "&#129689", lat: 0, lon: 0},
   ],
   3: [ // Wednesday
-    // RIDGELAND
-      { name: "Open Mind", deal: "&#9889 &#127855 20% off Vapes & Concentrates", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.36077, lon: -90.14643},
-      { name: "Ignite", deal: "&#127968 20% off budtender's choice", lat: 32.3766, lon: -90.1472},
-      { name: "Southern Sky - Lakeland", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.3329, lon: -90.1386}
+        // RIDGELAND
+          { name: "Open Mind", deal: "&#9889 &#127855 20% off Vapes & Concentrates", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.36077, lon: -90.14643},
+          { name: "Ignite", deal: "&#127968 20% off budtender's choice", lat: 32.3766, lon: -90.1472},
+          { name: "Southern Sky - Lakeland", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.33369, lon: -90.1446},
+          { name: "", deal: "", lat: 0, lon: 0},
   ],
   4: [ // Thursday
-    // RIDGELAND
-      { name: "Open Mind", deal: "&#129524 20% off Topicals", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.36077, lon: -90.14643},
-      { name: "Ignite", deal: "&#127865 BOGO 1/2 off Seltzer", lat: 32.3766, lon: -90.1472},
-      { name: "Southern Sky - Lakeland", deal: "&#128684 20% off Pre-rolls", lat: 32.3329, lon: -90.1386}
+        // RIDGELAND
+          { name: "Open Mind", deal: "&#129524 20% off Topicals", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.36077, lon: -90.14643},
+          { name: "Ignite", deal: "&#127865 BOGO 1/2 off Seltzer", lat: 32.3766, lon: -90.1472},
+          { name: "Southern Sky - Lakeland", deal: "&#128684 20% off Pre-rolls", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.33369, lon: -90.1446},
+          { name: "", deal: "", lat: 0, lon: 0},
   ],
   5: [ // Friday
-    // RIDGELAND
-      { name: "Open Mind", deal: "&#127804 $10 off each Flower item", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#127968 House Deal", lat: 32.36077, lon: -90.14643},
-      { name: "Ignite", deal: "&#127804 20% off all Flower", lat: 32.3766, lon: -90.1472},
-      { name: "Southern Sky - Lakeland", deal: "&#127804 20% off Flower", lat: 32.3329, lon: -90.1386}
+        // RIDGELAND
+          { name: "Open Mind", deal: "&#127804 $10 off each Flower item", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#127968 House Deal", lat: 32.36077, lon: -90.14643},
+          { name: "Ignite", deal: "&#127804 20% off all Flower", lat: 32.3766, lon: -90.1472},
+          { name: "Southern Sky - Lakeland", deal: "&#127804 20% off Flower", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#127968 House Deal", lat: 32.33369, lon: -90.1446},
+          { name: "", deal: "", lat: 0, lon: 0},
   ],
   6: [ // Saturday
-    // RIDGELAND
-      { name: "Open Mind", deal: "&#128092 20% off Accessories", lat: 32.38819, lon:-90.11402},
-      { name: "Rootdown - Old Canton", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.36077, lon: -90.14643},
-      // Ignite need to check on Saturday
-      { name: "Southern Sky - Lakeland", deal: "&#127855 20% off Concentrates", lat: 32.3329, lon: -90.1386}
+        // RIDGELAND
+          { name: "Open Mind", deal: "&#128092 20% off Accessories", lat: 32.38819, lon:-90.11402},
+          { name: "Rootdown - Old Canton", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.36077, lon: -90.14643},
+          // Ignite need to check on Saturday
+          { name: "Southern Sky - Lakeland", deal: "&#127855 20% off Concentrates", lat: 32.3329, lon: -90.1386},
+          { name: "Rootdown - Lakeland", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.33369, lon: -90.14466},
+          { name: "", deal: "", lat: 0, lon: 0},
   ]  
   
   
   // template { name: "", deal: "", lat: 0, lon: 0},
-  // house: &#127968 flower: &#127804 concentrate: &#127855 chocolate bar: &#127851 topical : &#129524 tincture : &#128167 
-  // pre-roll: &#128684 vape: &#9889 accessories: &#128092 beverages: &#127865
+  // house: &#127968 flower: &#127804 concentrate: &#127855 chocolate bar: &#127851 topical: &#129524 tincture: &#128167 
+  // pre-roll: &#128684 vape: &#9889 accessories: &#128092 beverages: &#127865 rewards points: &#129689
 };
 
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -104,5 +122,3 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
 }
-
-
