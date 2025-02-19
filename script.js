2facfd97ce5f7e1c486846e98abd5ddb0cfa40d2
Scheerhorn <daniel.scheerhorn.wolfe@gmail.com>
2025-02-19 03:27:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,120 @@
 const dailyDeals = {
   0: [ // Sunday
-        //CENTRAL
+        // CENTRAL
           // RIDGELAND 
-            { name: "Open Mind", deal: "&#127968 15% off store-wide", lat: 32.38819, lon:-90.11402},
+            {name: "Open Mind", deal: "&#127968 15% off store-wide", lat: 32.38819, lon:-90.11402},
             // Rootdown - Old Canton is closed on Sunday
-            // Ignite need to check on Sunday 
+            // Ignite need to check on Sunday 2/17
             // Southern Sky - Lakeland is closed on Sunday
             // Rootdown - Lakeland is closed on Sunday
-          //PEARL
-            { name: "Southern Sky - Pearl", deal: "&#10071 2 X loyalty points", lat: 32.26664, lon: -90.13920},
-          //RICHLAND
-            { name: "Oddfellow Wellness", deal: "&#127968 40-50% off select items", lat: 32.22353, lon: -90.15655},          
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#10071 2 X loyalty points", lat: 32.26664, lon: -90.13920},
+            // Voodoo Buds need to check if they have weekend deals 2/18
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127968 40-50% off select items", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#127968 15% off Store", lat: 32.38430, lon: -90.04180},
   ],
   1: [ // Monday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#127851 20% off Edibles", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#127804 20% off select micros Flower", lat: 32.36077, lon: -90.14643},
-          { name: "Ignite", deal: "&#127851 20% off Edibles", lat: 32.3766, lon: -90.1472},
-          { name: "Southern Sky - Lakeland", deal: "&#127851 20% off Edibles", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#127804 20% off select micros Flower", lat: 32.33369, lon: -90.1446},
-        //PEARL
-          { name: "Southern Sky - Pearl", deal: "&#127851 20% off edibles", lat: 32.26664, lon: -90.13920},
-        //RICHLAND
-          { name: "Oddfellow Wellness", deal: "&#127851 10% off edibles ", lat: 32.22353, lon: -90.15655},
+        // CENTRAL 
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#127851 20% off Edibles", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#127804 20% off select micros Flower", lat: 32.36077, lon: -90.14643},
+            {name: "Ignite", deal: "&#127851 20% off Edibles", lat: 32.3766, lon: -90.1472},
+            {name: "Southern Sky - Lakeland", deal: "&#127851 20% off Edibles", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#127804 20% off select micros Flower", lat: 32.33369, lon: -90.1446},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#127851 20% off edibles", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "&#127968 10% off Store 4-9pm", lat: 32.28377, lon: -90.11204},
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127851 10% off edibles ", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#127851 20% off Edibles", lat: 32.38430, lon: -90.04180},
   ],
   2: [ // Tuesday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#128684 20% off Pre-rolls", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.36077, lon: -90.14643},
-          { name: "Ignite", deal: "&#128167 20% off all Tincture", lat: 32.3766, lon: -90.1472},
-          { name: "Southern Sky - Lakeland", deal: "&#9889 20% off Vapes", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.33369, lon: -90.1446},
-        //PEARL
-          { name: "Southern Sky - Pearl", deal: "&#9889 20% off Vape Carts", lat: 32.26664, lon: -90.13920},
-        //RICHLAND
-          { name: "Oddfellow Wellness", deal: "Update deal", lat: 32.22353, lon: -90.15655},
+        // CENTRAL
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#128684 20% off Pre-rolls", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.36077, lon: -90.14643},
+            {name: "Ignite", deal: "&#128167 20% off all Tincture", lat: 32.3766, lon: -90.1472},
+            {name: "Southern Sky - Lakeland", deal: "&#9889 20% off Vapes", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#127851 Buy 2 get 1 100mg Gummies", lat: 32.33369, lon: -90.1446},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#9889 20% off Vape Carts", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "&#9889 15% off Carts", lat: 32.28377, lon: -90.11204},
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "Update deal", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#128684 20% off Pre-rolls", lat: 32.38430, lon: -90.04180},
   ],
   3: [ // Wednesday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#9889 &#127855 20% off Vapes & Concentrates", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.36077, lon: -90.14643},
-          { name: "Ignite", deal: "&#127968 20% off budtender's choice", lat: 32.3766, lon: -90.1472},
-          { name: "Southern Sky - Lakeland", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.33369, lon: -90.1446},
-        // PEARL
-          { name: "Southern Sky - Pearl", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.26664, lon: -90.13920},
-        // RICHLAND
-          { name: "Oddfellow Wellness", deal: "&#127855 10% off Concentrates", lat: 32.22353, lon: -90.15655},
+        // CENTRAL
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#9889 &#127855 20% off Vapes & Concentrates", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.36077, lon: -90.14643},
+            {name: "Ignite", deal: "&#127968 20% off budtender's choice", lat: 32.3766, lon: -90.1472},
+            {name: "Southern Sky - Lakeland", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#127855 BOGO 30% off Concentrates", lat: 32.33369, lon: -90.1446},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#129524 &#128167 20% off Topicals & Tinctures", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "&#128684 20% off Pre-Rolls", lat: 32.28377, lon: -90.11204},
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127855 10% off Concentrates", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#9889 &#127855 20% off Vapes & Concentrates", lat: 32.38430, lon: -90.04180},
   ],
   4: [ // Thursday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#129524 20% off Topicals", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.36077, lon: -90.14643},
-          { name: "Ignite", deal: "&#127865 BOGO 1/2 off Seltzer", lat: 32.3766, lon: -90.1472},
-          { name: "Southern Sky - Lakeland", deal: "&#128684 20% off Pre-rolls", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.33369, lon: -90.1446},
-        // PEARL
-          { name: "Southern Sky - Pearl", deal: "&#128684 20% off Pre-rolls", lat: 32.26664, lon: -90.13920},
-        // RICHLAND
-          { name: "Oddfellow Wellness", deal: "&#127968 Budtender's choice", lat: 32.22353, lon: -90.15655},
+        // CENTRAL
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#129524 20% off Topicals", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.36077, lon: -90.14643},
+            {name: "Ignite", deal: "&#127865 BOGO 1/2 off Seltzer", lat: 32.3766, lon: -90.1472},
+            {name: "Southern Sky - Lakeland", deal: "&#128684 20% off Pre-rolls", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#127865 Buy 3 get 1 Noble Seltzer", lat: 32.33369, lon: -90.1446},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#128684 20% off Pre-rolls", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "&#127968 10% off Store 4-9pm", lat: 32.28377, lon: -90.11204},
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127968 Budtender's choice", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#129524 20% off Topicals", lat: 32.38430, lon: -90.04180},
   ],
   5: [ // Friday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#127804 $10 off each Flower item", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#127968 House Deal", lat: 32.36077, lon: -90.14643},
-          { name: "Ignite", deal: "&#127804 20% off all Flower", lat: 32.3766, lon: -90.1472},
-          { name: "Southern Sky - Lakeland", deal: "&#127804 20% off Flower", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#127968 House Deal", lat: 32.33369, lon: -90.1446},
-        // PEARL
-          { name: "Southern Sky - Pearl", deal: "&#127804 20% off Flower", lat: 32.26664, lon: -90.13920},
-        //RICHLAND
-          { name: "Oddfellow Wellness", deal: "&#127968 20% off store-wide", lat: 32.22353, lon: -90.15655},
+        // CENTRAL
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#127804 $10 off each Flower item", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#127968 House Deal", lat: 32.36077, lon: -90.14643},
+            {name: "Ignite", deal: "&#127804 20% off all Flower", lat: 32.3766, lon: -90.1472},
+            {name: "Southern Sky - Lakeland", deal: "&#127804 20% off Flower", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#127968 House Deal", lat: 32.33369, lon: -90.1446},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#127804 20% off Flower", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "&#127851 10% off all Edibles", lat: 32.28377, lon: -90.11204},
+          //RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127968 20% off store-wide", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#127804 $10 off each Flower item", lat: 32.38430, lon: -90.04180},
   ],
   6: [ // Saturday
-        // RIDGELAND
-          { name: "Open Mind", deal: "&#128092 20% off Accessories", lat: 32.38819, lon:-90.11402},
-          { name: "Rootdown - Old Canton", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.36077, lon: -90.14643},
-          // Ignite need to check on Saturday
-          { name: "Southern Sky - Lakeland", deal: "&#127855 20% off Concentrates", lat: 32.3329, lon: -90.1386},
-          { name: "Rootdown - Lakeland", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.33369, lon: -90.14466},
-        // PEARL
-          { name: "Southern Sky - Pearl", deal: "&#127855 20% off Concentrates", lat: 32.26664, lon: -90.13920},
-        //RICHLAND
-          { name: "Oddfellow Wellness", deal: "&#127968 40-50% off select items", lat: 32.22353, lon: -90.15655},
+        // CENTRAL
+          // RIDGELAND
+            {name: "Open Mind", deal: "&#128092 20% off Accessories", lat: 32.38819, lon:-90.11402},
+            {name: "Rootdown - Old Canton", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.36077, lon: -90.14643},
+            // Ignite need to check on Saturday
+            {name: "Southern Sky - Lakeland", deal: "&#127855 20% off Concentrates", lat: 32.3329, lon: -90.1386},
+            {name: "Rootdown - Lakeland", deal: "&#9889 BOGO 30% off full-spectrum Carts", lat: 32.33369, lon: -90.14466},
+          // PEARL
+            {name: "Southern Sky - Pearl", deal: "&#127855 20% off Concentrates", lat: 32.26664, lon: -90.13920},
+            {name: "Voodoo Buds", deal: "", lat: 32.28377, lon: -90.11204},
+            // Voodoo Buds need to check on weekend deals 
+          // RICHLAND
+            {name: "Oddfellow Wellness", deal: "&#127968 40-50% off select items", lat: 32.22353, lon: -90.15655},
+          // FLOWOOD
+            {name: "Spillway Dispensary", deal: "&#128092 20% off Accessories", lat: 32.38430, lon: -90.04180},
   ]  
   
   
-  // template { name: "", deal: "", lat: 0, lon: 0},
+  // template {name: "", deal: "", lat: 0, lon: 0},
   // house: &#127968 flower: &#127804 concentrate: &#127855 chocolate bar: &#127851 topical: &#129524 tincture: &#128167 
   // pre-roll: &#128684 vape: &#9889 accessories: &#128092 beverages: &#127865 rewards points: &#10071
 };
